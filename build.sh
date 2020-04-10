a900f097df7533e51dfce6e80bc0b22c28361831
132ikl <132@ikl.sh>
2020-04-10 04:07:11 -0400

    Add systemd service

--- build.sh
+++ build.sh
@@ -3,11 +3,14 @@
 export VERSION="$(cat VERSION)"
 
 cd package/
+rm -rf liteshort/ debian/liteshort.service
 
-rm -rf liteshort/
 git clone --branch feature/package https://github.com/132ikl/liteshort/
-mv liteshort/setup/liteshort.service ./
+cp liteshort/setup/liteshort.service debian/
 
 git log --no-walk --tags --pretty="liteshort (%S) unstable; urgency=medium%n%n  * See the full changelog at:%n  * https://github.com/132ikl/liteshort/releases/tag/v%S%n%n -- Steven Spangler <132@ikl.sh>  %cD%n" --decorate=off | sed 's/(v/(/g' | sed 's/\/v/\//g' > debian/changelog
 
 dpkg-buildpackage -us -uc --build=all
+
+dh clean
+rm -rf liteshort/ debian/liteshort.service
