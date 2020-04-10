6f44de486196702d40bacf195a7e65ce347f8d20
132ikl <132@ikl.sh>
2020-04-10 05:04:39 -0400

    Downgrade to upstream version

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ export VERSION="$(cat VERSION)"
 cd package/
 rm -rf liteshort/ debian/liteshort.service
 
-git clone --branch feature/package https://github.com/132ikl/liteshort/
+git clone https://github.com/132ikl/liteshort/
 cp liteshort/setup/liteshort.service debian/
 
 git log --no-walk --tags --pretty="liteshort (%S) unstable; urgency=medium%n%n  * See the full changelog at:%n  * https://github.com/132ikl/liteshort/releases/tag/v%S%n%n -- Steven Spangler <132@ikl.sh>  %cD%n" --decorate=off | sed 's/(v/(/g' | sed 's/\/v/\//g' > debian/changelog
